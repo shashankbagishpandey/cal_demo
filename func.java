e673a0142ff28df9239a15a62a9a8ccd0ccd8681
shashankpandey <shashankpandey3456@gmail.com>
2022-07-31 17:31:26 +0530

    added function of add n sub

---
+++ func.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class func{
+
+public static int  addition(int n1,int n2){
+
+int add=n1+n2;
+return add;
+
+}
+
+
+public static int  substraction(int n1,int n2){
+
+int sub=(n1-n2)*(-1);
+return sub;
+
+}
+
+
+}
